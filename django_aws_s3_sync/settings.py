102728846560d71e6c158414b707a6528b716fdd
ccortinhas <ccortinhas@ubiwhere.com>
2018-06-28 15:37:05 +0100

    configured some defaults

--- django_aws_s3_sync/settings.py
+++ django_aws_s3_sync/settings.py
@@ -1,12 +1,4 @@
 
-# import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-vu!m7x5qzh(^=cx_zxz=o6q52@_y(khhhp_bwvv4sxy0_ik5s'
-
 INSTALLED_APPS = [
     'django_aws_s3_sync',
 ]
